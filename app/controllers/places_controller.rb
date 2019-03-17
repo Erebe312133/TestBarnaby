19cc687036deb24c3c46f8374e7cbfcf3381cee2
Erebe <yanis.breton@hotmail.fr>
2019-03-17 15:02:39 +0100

    Implement google search for places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,13 +1,26 @@
+require 'rest-client'
+
 class PlacesController < ApplicationController
 
     def search
-        if params[:query].blank?
+        Rails.logger.info "1"
+        if params[:q].blank?
             render json: {errors:'error'}, status: 401
             return
         end
-        @client = GooglePlaces::Client.new("AIzaSyBYxH6hZs7QxFzDxSwcHBh7LCx-JUFkgHI")
-        @client.spots_by_query(params[:query])
-        render json: {place: @client.spots_by_query(params[:query])}, status: 201
+        
+        response = RestClient::Request.execute(
+            method: :get,
+            url: URI.escape("https://maps.googleapis.com/maps/api/place/autocomplete/json?input=#{params[:q]}&key=AIzaSyBYxH6hZs7QxFzDxSwcHBh7LCx-JUFkgHI")
+        )
+        json = JSON.parse response
+        response = RestClient::Request.execute(
+            method: :get,
+            url: URI.escape("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=#{json['predictions'][0]['description']}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyBYxH6hZs7QxFzDxSwcHBh7LCx-JUFkgHI")
+        )
+        json = JSON.parse response
+
+        render json: {google_places: json['candidates']}, status: 201
     end
 
 end
