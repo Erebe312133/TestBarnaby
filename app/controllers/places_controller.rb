1c8b77c8dea028c3c82280145405f046fc5edc12
Erebe <yanis.breton@hotmail.fr>
2019-03-17 16:41:13 +0100

    Change google api call

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,15 +8,9 @@ class PlacesController < ApplicationController
             render json: {errors:'error'}, status: 401
             return
         end
-        
         response = RestClient::Request.execute(
             method: :get,
-            url: URI.escape("https://maps.googleapis.com/maps/api/place/autocomplete/json?input=#{params[:q]}&key=AIzaSyBYxH6hZs7QxFzDxSwcHBh7LCx-JUFkgHI")
-        )
-        json = JSON.parse response
-        response = RestClient::Request.execute(
-            method: :get,
-            url: URI.escape("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=#{json['predictions'][0]['description']}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyBYxH6hZs7QxFzDxSwcHBh7LCx-JUFkgHI")
+            url: URI.escape("https://maps.googleapis.com/maps/api/place/textsearch/json?query=#{params[:q]}&key=AIzaSyBYxH6hZs7QxFzDxSwcHBh7LCx-JUFkgHI&type=bar")
         )
         json = JSON.parse response
 
