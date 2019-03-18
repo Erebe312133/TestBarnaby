7f8d7580f2d21025a1c0083a643af7ba35c43c34
Erebe <yanis.breton@hotmail.fr>
2019-03-18 07:02:43 +0100

    Last change on scope

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,5 +2,5 @@ class Place < ApplicationRecord
     has_many :opening_hours
     has_one :coordinates, class_name: 'Coordinate'
 
-    scope :by_matching_name, ->(name) { where("lower(name) = ?", "%#{name.downcase}%") }
+    scope :by_matching_name, ->(name) { where("lower(name) LIKE ?", "%#{name.downcase}%") }
 end
