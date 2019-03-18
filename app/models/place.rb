8182a2f5e95b5724b1a88082c868de04ae48471f
Erebe <yanis.breton@hotmail.fr>
2019-03-18 06:49:49 +0100

    Add a scope to find bar by almost matching name

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ApplicationRecord
     has_many :opening_hours
     has_one :coordinates, class_name: 'Coordinate'
+
+    scope :matching_name, ->(name) { where("levenshtein(name, ?) < 3", name) }
 end
