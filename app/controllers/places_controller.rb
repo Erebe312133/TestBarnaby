94c611c353d065552f4fee2edd959a6aa5d7d016
Erebe <yanis.breton@hotmail.fr>
2019-03-19 14:00:38 +0100

    Find barnaby bar with coordinate

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,7 +7,8 @@ class PlacesController < ApplicationController
             render json: {errors:'error'}, status: 401
             return
         end
-        @barnaby_places = Place.by_matching_name(params[:q])
+        @barnaby_places = []
+        Place.by_matching_name(params[:q]).each { |place| @barnaby_places << place}
         get_place_with_google
         render json: {google_places: @google_places, barnaby_places: @barnaby_places}, status: 201
     end
@@ -27,8 +28,7 @@ private
                 url: URI.escape("https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyBYxH6hZs7QxFzDxSwcHBh7LCx-JUFkgHI&placeid=#{row['place_id']}&fields=alt_id,formatted_address,geometry,icon,id,name,permanently_closed,place_id,plus_code,scope,type,url,user_ratings_total,utc_offset,vicinity")
             )
             json_place = JSON.parse response_place
-            logger.debug "here" if json_place.key? 'geometry'
-            get_bar_in_place json_place['result']['geometry']['location'] if json_place.key?('result') && json_place['result']['types'].include?('locality')
+            get_bar_in_place json_place['result']['geometry'] if json_place.key?('result') && json_place['result']['types'].include?('locality')
             @google_places << json_place['result'] if json_place.key?('result') && json_place['result']['types'].include?('bar')
         end
     end
@@ -36,7 +36,7 @@ private
     def get_bar_in_place(coordinate)
         response = RestClient::Request.execute(
             method: :get,
-            url: URI.escape("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{coordinate['lat']},#{coordinate['lng']}&radius=1500&type=bar&key=AIzaSyBYxH6hZs7QxFzDxSwcHBh7LCx-JUFkgHI")
+            url: URI.escape("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{coordinate['location']['lat']},#{coordinate['location']['lng']}&radius=1500&type=bar&key=AIzaSyBYxH6hZs7QxFzDxSwcHBh7LCx-JUFkgHI")
         )
         json = JSON.parse response
         json['results'][0..4].each do |row|
@@ -47,6 +47,10 @@ private
             json_place = JSON.parse response_place
             @google_places << json_place['result'] if json_place.key?('result') && json_place['result']['types'].include?('bar')
         end
+        coordinate['viewport']['southwest'] = [coordinate['viewport']['southwest']['lat'], coordinate['viewport']['southwest']['lng']]
+        coordinate['viewport']['northeast'] = [coordinate['viewport']['northeast']['lat'], coordinate['viewport']['northeast']['lng']]
+        coordinate['location'] = [coordinate['location']['lat'], coordinate['location']['lng']]
+        Coordinate.in_bounds([coordinate['viewport']['southwest'], coordinate['viewport']['northeast']], :origin => coordinate['location']).each { |coord| @barnaby_places << coord.place }
     end
 
 end
